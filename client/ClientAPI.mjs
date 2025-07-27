69af26d9794517658b968c21420fc549c98bfa37
Christian R <github@chrisnew.de>
2025-07-27 13:48:28 +0300

    commented out drawing of debug info

--- client/ClientAPI.mjs
+++ client/ClientAPI.mjs
@@ -36,21 +36,21 @@ export class ClientGameAPI {
   }
 
   draw() {
-    /** @type {{[key: string]: number}} */
-    const entities = {};
+    // /** @type {{[key: string]: number}} */
+    // const entities = {};
 
-    for (const entity of this.engine.GetVisibleEntities()) {
-      entities[entity.classname] = (entities[entity.classname] || 0) + 1;
-    }
+    // for (const entity of this.engine.GetVisibleEntities()) {
+    //   entities[entity.classname] = (entities[entity.classname] || 0) + 1;
+    // }
 
-    const sortedEntities = Object.entries(entities).sort((a, b) => b[1] - a[1]);
+    // const sortedEntities = Object.entries(entities).sort((a, b) => b[1] - a[1]);
 
-    for (let i = 0; i < sortedEntities.length && i < 10; i++) {
-      const [classname, count] = sortedEntities[i];
-      this.engine.DrawString(32, 32 + i * 16, `${count.toFixed(0).padStart(3)} ${classname}`, 1.5);
-    }
+    // for (let i = 0; i < sortedEntities.length && i < 10; i++) {
+    //   const [classname, count] = sortedEntities[i];
+    //   this.engine.DrawString(32, 32 + i * 16, `${count.toFixed(0).padStart(3)} ${classname}`, 1.5);
+    // }
 
-    this.engine.DrawString(32 + 400, 32, `Time: ${this.engine.CL.time.toFixed(3)}`, 1.5);
+    // this.engine.DrawString(32 + 400, 32, `Time: ${this.engine.CL.time.toFixed(3)}`, 1.5);
   }
 
 
